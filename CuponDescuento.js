242857dd6a8dcb9e751c7d91b00b29714dea1fe2
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-16 11:58:11 -0300

    los cupones funcioan con un objeto que contine toda la lista de cupones

--- CuponDescuento.js
+++ CuponDescuento.js
@@ -1,38 +1,46 @@
-const precio = document.getElementById('input-precio');
-const coupon = document.getElementById('input-coupon');
-const nuevoPrecio = document.getElementById('nuevo-valor');
-const btnCalcular = document.getElementById('btn-calcular');   
+const precio = document.getElementById("input-precio");
+const coupon = document.getElementById("input-coupon");
+const nuevoPrecio = document.getElementById("nuevo-valor");
+const btnCalcular = document.getElementById("btn-calcular");
 
 // precio.addEventListener('input', calcularPrecioConDescuento);
 // descuento.addEventListener('input', calcularPrecioConDescuento);
 
-btnCalcular.addEventListener('click', calcularPrecioConDescuento);
+btnCalcular.addEventListener("click", calcularPrecioConDescuento);
 
 let descuntosValidos = {
-    'platzi': 15,
-    'cupon1': 10,
-    'cupon2': 20,
-    'cupon3': 30
-}
+	platzi: 15,
+	cupon1: 10,
+	cupon2: 20,
+	cupon3: 30,
+};
 
 function calcularPrecioConDescuento() {
-    let precioIngresado = precio.value;
-    precioIngresado = parseInt(precioIngresado);
-    let couponIngresado = coupon.value;
+	let precioIngresado = precio.value;
+	precioIngresado = parseInt(precioIngresado);
+	let couponIngresado = coupon.value;
 
-    if (!descuntosValidos[couponIngresado]) {
-        nuevoPrecio.innerText = 'Cupón no válido';
-        return;
-    }
-
-    let descuentoIngresado = descuntosValidos[couponIngresado];
-
+    let descuentoIngresado = 0;
+    
+    console.log({
+        precioIngresado,
+        couponIngresado,
+        descuentoIngresado,
+    }) 
+    
     if (!precioIngresado) {
-        nuevoPrecio.innerText = 'Ingrese un precio válido';
+        nuevoPrecio.innerText = "Ingrese un precio válido";
+		return;
+	}
+    
+    if (!descuntosValidos[couponIngresado]) {
+        nuevoPrecio.innerText = "Cupón no válido";
         return;
     }
 
-    let precioConDescuento = (precioIngresado * (100 - descuentoIngresado)) / 100;
+    descuentoIngresado = descuntosValidos[couponIngresado];
+    
+	let precioConDescuento = (precioIngresado * (100 - descuentoIngresado)) / 100;
 
-    nuevoPrecio.innerText = `$${precioConDescuento}`;
+	nuevoPrecio.innerText = `$${precioConDescuento}`;
 }
