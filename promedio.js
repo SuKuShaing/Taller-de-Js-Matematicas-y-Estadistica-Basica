1bf0dc797c6428b9700a7efc9895b0d1da235dd2
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-17 13:43:52 -0300

    sea añade la funcionalidad para sacar el la Mediana

--- promedio.js
+++ promedio.js
@@ -1,7 +1,8 @@
 const input = document.getElementsByTagName('input')
 const button = document.getElementsByTagName('button')
 const listaDeNumeros = document.getElementsByTagName('p')
-const resultado = document.getElementsByTagName('span')
+const resultadoPromedio = document.getElementsByTagName('span')
+const resultadoMediana = document.getElementById('resultado-Mediana')
 
 let listaDeNumerosIngresados = []
 
@@ -9,19 +10,37 @@ button[0].addEventListener('click', () => {
     listaDeNumerosIngresados.push(Number(input[0].value))
     input[0].value = '' // Limpia el input
 
-    rendeditarLista()
-    promediar()
+    renderizarLista()
+    promediar(listaDeNumerosIngresados)
+    calcularMediana()
 })
 
-function rendeditarLista() {
+function renderizarLista() {
     listaDeNumeros[0].innerHTML = ''
     listaDeNumerosIngresados.forEach(numero => {
         listaDeNumeros[0].innerHTML += `${numero}, `
     })
 }
 
-function promediar() {
-    const suma = listaDeNumerosIngresados.reduce((acc, numero) => acc + numero, 0)
-    const promedio = suma / listaDeNumerosIngresados.length
-    resultado[0].innerHTML = promedio.toFixed(2)
+function promediar(lista) {
+    const suma = lista.reduce((acc, numero) => acc + numero, 0)
+    const promedio = suma / lista.length
+    resultadoPromedio[0].innerHTML = promedio.toFixed(2)
+    return promedio
+}
+
+function calcularMediana() {
+    if (listaDeNumerosIngresados.length % 2 === 0) {
+        const mitadLista = listaDeNumerosIngresados.length / 2
+        const elemento1 = listaDeNumerosIngresados[mitadLista - 1]
+        const elemento2 = listaDeNumerosIngresados[mitadLista]
+        const numCentrales = [elemento1, elemento2]
+
+        const promedioElementosCentrales = promediar(numCentrales)
+        resultadoMediana.innerHTML = `${promedioElementosCentrales}`
+    } else {
+        const mitadLista = Math.floor(listaDeNumerosIngresados.length / 2)
+        const mediana = listaDeNumerosIngresados[mitadLista]
+        resultadoMediana.innerHTML = `${mediana}`
+    }
 }
